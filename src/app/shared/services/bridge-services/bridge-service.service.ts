f544d7dd08171f8b026e9d70ca71402e0e8db074
parraluisa <parran_ll@javeriana.edu.co>
2024-05-07 01:31:18 -0500

    bridege service updated

--- src/app/shared/services/bridge-services/bridge-service.service.ts
+++ src/app/shared/services/bridge-services/bridge-service.service.ts
@@ -1,9 +1,40 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {bridge} from "../../../models/bridge/bridge";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BridgeServiceService {
 
-  constructor() { }
+  constructor() {
+  }
+
+  generarDatosQuemados(): bridge {
+    return this.generarPuente();
+  }
+
+  generarPuente(): bridge {
+    return {
+      idBridge: 1,
+      name: 'Puente de ejemplo',
+      regionalId: 1,
+      roadId: 123,
+      bridgeId: 123,
+      road: 'Nombre de la carretera',
+      pr: 'PR123',
+      inspections: [],
+      inventory: {
+        idInventory: 0,
+        year: 0,
+        length: 0,
+        width: 0,
+        height: 0,
+        material: '',
+        condition: '',
+        damage: '',
+        photo: ''
+      },
+      municipality: ''
+    };
+  }
 }
