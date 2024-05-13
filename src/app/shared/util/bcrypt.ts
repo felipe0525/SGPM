a4f7c31815515b4272ab10fb31f2017dcc3b65e0
Daniel Vargas <dvargasp@javeriana.edu.co>
2024-05-13 06:49:01 -0500

    Modify README.md

---
+++ src/app/shared/util/bcrypt.ts
@@ -0,0 +1,12 @@
+/*
+
+import bcrypt from 'bcrypt';
+
+const saltRounds = 10;
+
+export function hashPw(plaintextPassword: string) {
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hash = bcrypt.hashSync(plaintextPassword, salt);
+    return hash;
+}
+*/
