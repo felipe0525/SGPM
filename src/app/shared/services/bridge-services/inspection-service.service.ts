ff3ed46ce7ce0856de67857ab665df48c35fdd43
parraluisa <parran_ll@javeriana.edu.co>
2024-05-14 10:59:40 -0500

    inspection Service updated

--- src/app/shared/services/bridge-services/inspection-service.service.ts
+++ src/app/shared/services/bridge-services/inspection-service.service.ts
@@ -1,233 +1,169 @@
-import { Injectable, inject } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
 import {
-  Firestore,
   addDoc,
   collection,
   collectionData,
-  deleteDoc,
   doc,
-  getDoc,
+  DocumentData,
+  DocumentReference,
+  Firestore, getDoc,
   getDocs,
   query,
-  updateDoc,
-  where,
+  where
 } from '@angular/fire/firestore';
-import {Observable, of} from "rxjs";
+import {from, map, Observable, of, switchMap} from "rxjs";
+import {Inspection} from "../../../models/bridge/inspection";
 
 
-const PATH = 'bridge';
+const PATH = 'inventories';
+
 @Injectable({
   providedIn: 'root'
 })
 export class InspectionServiceService {
 
   private _firestore = inject(Firestore);
+
   private _collection = collection(this._firestore, PATH);
   inspection: any;
 
-  getInspections(bridgeId: number):Observable<any> {
-    //return collectionData(this._collection, { idField: 'id' }) as Observable<any>;
-    return new Observable<any>(subscriber => {
-      subscriber.next([
-        {
-          inspectionId: 1,
-          date: 25-10-2021,
-          temperature: 10,
-          inspector: 'roberto',
-          administrator: 'camilo',
-          nextInspectionYear: 2024,
-          bridgeSurfaceName: 'jkdsf',
-          inspectionComponents: [
-            {
-              componentName: 'Superficie del puente',
-              rating: 0,
-              damageType: 10,
-              repairType: 'A',
-              repairQuantity: 10,
-              repairUnit: 'm2',
-              generalComments: 'no hay comentarios',
-            },
-            {
-              componentName: 'Juntas de expansión',
-              rating: 0,
-              damageType: 10,
-              repairType: 'A',
-              repairQuantity: 10,
-              repairUnit: 'm2',
-              generalComments: 'no hay comentarios',
-            },
-            {
-              componentName: 'Andenes/ bordillos',
-              rating: 0,
-              damageType: 10,
-              repairType: 'A',
-              repairQuantity: 10,
-              repairUnit: 'm2',
-              generalComments: 'no hay comentarios',
-            },
-            {
-              componentName: 'Barandas',
-              rating: 0,
-              damageType: 10,
-              repairType: 'A',
-              repairQuantity: 10,
-              repairUnit: 'm2',
-              generalComments: 'no hay comentarios',
-            },
-            {
-              componentName: 'Conos/ taludes',
-              rating: 0,
-              damageType: 10,
-              repairType: 'A',
-              repairQuantity: 10,
-              repairUnit: 'm2',
-              generalComments: 'no hay comentarios',
-            },
-            {
-              componentName: 'Aletas',
-              rating: 0,
-              damageType: 10,
-              repairType: 'A',
-              repairQuantity: 10,
-              repairUnit: 'm2',
-              generalComments: 'no hay comentarios',
-            },
-            {
-              componentName: 'Estribos',
-              rating: 0,
-              damageType: 10,
-              repairType: 'A',
-              repairQuantity: 10,
-              repairUnit: 'm2',
-              generalComments: 'no hay comentarios',
-            },
-            {
-              componentName: 'Pilas',
-              rating: 0,
-              damageType: 10,
-              repairType: 'A',
-              repairQuantity: 10,
-              repairUnit: 'm2',
-              generalComments: 'no hay comentarios',
-            },
-            {
-              componentName: 'Apoyos',
-              rating: 0,
-              damageType: 10,
-              repairType: 'A',
-              repairQuantity: 10,
-              repairUnit: 'm',
-              generalComments: 'no hay comentarios'},
-          ],
-          generalComments: 'no hay comentarios',
-        },
-        {
-          inspectionId: 2,
-          date: 25-10-2021,
-          temperature: 10,
-          inspector: 'roberto',
-          administrator: 'camilo',
-          nextInspectionYear: 2024,
-          bridgeSurfaceName: 'jkdsf',
-          inspectionComponents: [
-            {
-              componentName: 'Superficie del puente',
-              rating: 0,
-              damageType: 10,
-              repairType: 'B',
-              repairQuantity: 10,
-              repairUnit: 'm2',
-              generalComments: 'no hay comentarios',
-            },
-            {
-              componentName: 'Juntas de expansión',
-              rating: 0,
-              damageType: 10,
-              repairType: 'C',
-              repairQuantity: 10,
-              repairUnit: 'm2',
-              generalComments: 'no hay comentarios',
-            },
-            {
-              componentName: 'Andenes/ bordillos',
-              rating: 0,
-              damageType: 10,
-              repairType: 'K',
-              repairQuantity: 10,
-              repairUnit: 'm2',
-              generalComments: 'no hay comentarios',
-            },
-            {
-              componentName: 'Barandas',
-              rating: 0,
-              damageType: 10,
-              repairType: 'A',
-              repairQuantity: 10,
-              repairUnit: 'm2',
-              generalComments: 'no hay comentarios',
-            },
-            {
-              componentName: 'Conos/ taludes',
-              rating: 0,
-              damageType: 10,
-              repairType: 'A',
-              repairQuantity: 10,
-              repairUnit: 'm2',
-              generalComments: 'no hay comentarios',
-            },
-            {
-              componentName: 'Aletas',
-              rating: 0,
-              damageType: 10,
-              repairType: 'A',
-              repairQuantity: 10,
-              repairUnit: 'm2',
-              generalComments: 'no hay comentarios',
-            },
-            {
-              componentName: 'Estribos',
-              rating: 0,
-              damageType: 10,
-              repairType: 'A',
-              repairQuantity: 10,
-              repairUnit: 'm2',
-              generalComments: 'no hay comentarios',
-            },
-            {
-              componentName: 'Pilas',
-              rating: 0,
-              damageType: 10,
-              repairType: 'A',
-              repairQuantity: 10,
-              repairUnit: 'm2',
-              generalComments: 'no hay comentarios',
-            },
-            {
-              componentName: 'Apoyos',
-              rating: 0,
-              damageType: 10,
-              repairType: 'A',
-              repairQuantity: 10,
-              repairUnit: 'm',
-              generalComments: 'no hay comentarios'},
-          ],
-          generalComments: 'no hay comentarios',
+  getInspections(bridgeIdentification: number): Observable<any[]> {
+    const q = query(this._collection, where('generalInformation.bridgeIdentification', '==', bridgeIdentification));
+    return collectionData(q, {idField: 'id'}).pipe(
+      switchMap((inventories: any[]) => {
+        if (inventories.length === 0) {
+          return of([]);
+        } else {
+          const inventoryId = inventories[0].id;
+          const inventoryDocRef = doc(this._firestore, `${PATH}/${inventoryId}`);
+          const inspectionCollectionRef = collection(inventoryDocRef, 'inspections');
+          return collectionData(inspectionCollectionRef).pipe(
+            map(inspections => {
+              return inspections.map(inspection => ({
+                id: inspection['inspectionId'],
+                administrator: inspection['administrator'],
+                inspector: inspection['inspector'],
+                date: inspection['date']
+              }));
+            })
+          );
+
         }
-      ]);
-    });
+      })
+    );
   }
 
-  setInspection(inspection: any) {
-    this.inspection = inspection;
 
-  }
-  getInspectionByInspectionId(inspectionId: number | null) {
-    //return getDoc(doc(this._collection, inspectionId));
-    return of(this.inspection);
+  async setInspection(bridgeId: any, inspection: Inspection): Promise<DocumentReference<DocumentData>> {
+    const q = query(this._collection, where('generalInformation.bridgeIdentification', '==', bridgeId));
+    const inventoriesSnapshot = await getDocs(q);
+    const inventoryDocRef = inventoriesSnapshot.docs[0].ref;
+    const inspectionCollectionRef = collection(inventoryDocRef, 'inspections');
+    return addDoc(inspectionCollectionRef, inspection);
+    /*
+        // Subir la inspección a Firestore
+        const inspectionRef = await addDoc(inspectionCollectionRef, inspection);
+
+        // Subir fotos a Firebase Storage
+        await Promise.all(inspection.inspectionComponents.map(async (component, componentIndex) => {
+          await Promise.all(component.photos.map(async (photoUrl, photoIndex) => {
+            const photoBlob = await this.fetchPhotoBlob(photoUrl); // Función para obtener el blob de la foto
+            const storageRef = ref(getStorage(), `inspections/${inspection.inspectionId}/${componentIndex}/${photoIndex}`);
+            await uploadBytes(storageRef, photoBlob);
+          }));
+        }));
+
+        return inspectionRef;*/
   }
 
-  generateInspectionId() {
-    const number = Math.floor(Math.random() * 1000);
-    return of(number);
+
+  async getInspection(bridgeIdentification: any, inspectionId: any): Promise<Inspection | null> {
+    try {
+      // Buscar el inventario con el ID proporcionado
+      const inventoryQuery = query(this._collection, where('generalInformation.bridgeIdentification', '==', bridgeIdentification));
+      const inventorySnapshot = await getDocs(inventoryQuery);
+
+      // Verificar si se encontró el inventario
+      if (inventorySnapshot.empty) {
+        console.error('No se encontró ningún inventario con el ID proporcionado');
+        return null;
+      }
+
+      // Obtener la referencia al documento de inventario encontrado
+      const inventoryDocRef = inventorySnapshot.docs[0].ref;
+      console.log('Path al inventario:', inventoryDocRef.path);
+
+      // Obtener las inspecciones dentro del inventario
+      const inspectionCollectionRef = collection(inventoryDocRef, 'inspections');
+      console.log('Path a las inspecciones:', inspectionCollectionRef.path);
+
+
+      const inspectionsQuery = query(inspectionCollectionRef, where('inspectionId', '==', parseInt(inspectionId)));
+      const inspectionsSnapshot = await getDocs(inspectionsQuery);
+
+      // Verificar si se encontró alguna inspección con el ID proporcionado
+      if (inspectionsSnapshot.empty) {
+        console.error('No se encontró ninguna inspección con el ID proporcionado');
+        return null;
+      }
+
+      // Obtener el primer documento de inspección encontrado (asumiendo que solo hay una coincidencia)
+      const inspectionData = inspectionsSnapshot.docs[0].data();
+      console.log('Información de la inspección:', inspectionData);
+
+      // Convertir los datos a los tipos definidos en el modelo
+      const inspection: Inspection = {
+        inspectionId: inspectionData['inspectionId'],
+        date: inspectionData['date'], // Convertir el timestamp a Date
+        temperature: inspectionData['temperature'],
+        inspector: inspectionData['inspector'],
+        administrator: inspectionData['administrator'],
+        nextInspectionYear: inspectionData['nextInspectionYear'],
+        inspectionComponents: inspectionData['inspectionComponents'],
+        generalComments: inspectionData['generalComments']
+      };
+
+      return inspection;
+    } catch (error) {
+      console.error('Error al obtener la inspección:', error);
+      return null;
+    }
   }
+
+
+
+  async generateInspectionId(): Promise<number | null> {
+    let id: number;
+    let repeat: boolean;
+
+    do {
+      id = Math.floor(Math.random() * 1000000);
+      repeat = await this.checkIfIdExists(id);
+    } while (repeat);
+
+    return id;
+  }
+
+  async checkIfIdExists(id: number): Promise<boolean> {
+    try {
+      const inventoriesSnapshot = await getDocs(this._collection);
+
+      for (const inventoryDoc of inventoriesSnapshot.docs) {
+        const inspectionCollectionRef = collection(inventoryDoc.ref, 'inspections');
+        const inspectionsSnapshot = await getDocs(inspectionCollectionRef);
+
+        for (const inspectionDoc of inspectionsSnapshot.docs) {
+          if (inspectionDoc.id === id.toString()) {
+            return true;
+          }
+        }
+      }
+
+      return false;
+    } catch (error) {
+      console.error('Error al verificar la existencia del ID de inspección:', error);
+      return true;
+    }
+  }
+
 }
