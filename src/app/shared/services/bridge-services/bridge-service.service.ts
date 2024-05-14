05997b7dd4c3ba20a9a5b0ca4bb1dc219638788b
parraluisa <parran_ll@javeriana.edu.co>
2024-05-13 19:07:49 -0500

    bridge service updated for verifications

--- src/app/shared/services/bridge-services/bridge-service.service.ts
+++ src/app/shared/services/bridge-services/bridge-service.service.ts
@@ -1,10 +1,12 @@
 import {Injectable} from '@angular/core';
 import {bridge} from "../../../models/bridge/bridge";
+import {Observable, of} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BridgeServiceService {
+  private inspection: any;
 
   constructor() {
   }
@@ -36,5 +38,37 @@ export class BridgeServiceService {
       },
       municipality: ''
     };
+  }
+
+  getBridgeInfo() {
+    return {
+      idBridge: 1,
+      name: 'Puente de ejemplo',
+      regionalId: 1,
+      roadId: 123,
+      bridgeId: 123,
+      road: 'Nombre de la carretera',
+      pr: 'PR123',
+    };
+  }
+
+  getBridgeName(bridgeId: number):Observable<string> {
+    return new Observable<string>(subscriber => {
+      subscriber.next('Puente de ejemplo');
+    });
+
+  }
+
+  getBridgeBasicInfo(bridgeId: any):Observable<bridge> {
+    /*return new Observable<bridge>(subscriber => {
+      subscriber.next(this.generarPuente());
+    });*/
+    const bridge = this.generarPuente();
+    return of(bridge)
+  }
+
+  sendInspection(inspection: any) {
+    console.log(inspection);
+    this.inspection = inspection;
   }
 }
