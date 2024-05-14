80d929168b2bb04fa61da7f71b3f7ec13316ebf3
parraluisa <parran_ll@javeriana.edu.co>
2024-05-14 10:59:28 -0500

    bridge Service updated

--- src/app/shared/services/bridge-services/bridge-service.service.ts
+++ src/app/shared/services/bridge-services/bridge-service.service.ts
@@ -1,70 +1,41 @@
-import {Injectable} from '@angular/core';
-import {bridge} from "../../../models/bridge/bridge";
+import { Injectable, inject } from '@angular/core';
+import {
+  Firestore,
+  addDoc,
+  collection,
+  collectionData,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  updateDoc,
+  where,
+} from '@angular/fire/firestore';
 import {Observable, of} from "rxjs";
 
+const PATH = 'bridge';
 @Injectable({
   providedIn: 'root'
 })
 export class BridgeServiceService {
+  private _firestore = inject(Firestore);
+  private _collection = collection(this._firestore, PATH);
   private inspection: any;
 
   constructor() {
   }
 
-  generarDatosQuemados(): bridge {
-    return this.generarPuente();
-  }
-
-  generarPuente(): bridge {
-    return {
-      idBridge: 1,
-      name: 'Puente de ejemplo',
-      regionalId: 1,
-      roadId: 123,
-      bridgeId: 123,
-      road: 'Nombre de la carretera',
-      pr: 'PR123',
-      inspections: [],
-      inventory: {
-        idInventory: 0,
-        year: 0,
-        length: 0,
-        width: 0,
-        height: 0,
-        material: '',
-        condition: '',
-        damage: '',
-        photo: ''
-      },
-      municipality: ''
-    };
-  }
-
-  getBridgeInfo() {
-    return {
-      idBridge: 1,
-      name: 'Puente de ejemplo',
-      regionalId: 1,
-      roadId: 123,
-      bridgeId: 123,
-      road: 'Nombre de la carretera',
-      pr: 'PR123',
-    };
-  }
-
   getBridgeName(bridgeId: number):Observable<string> {
-    return new Observable<string>(subscriber => {
-      subscriber.next('Puente de ejemplo');
-    });
-
+    //Get the bridge from database and return the name
+  return of("Bridge Name");
   }
 
-  getBridgeBasicInfo(bridgeId: any):Observable<bridge> {
+  getBridgeBasicInfo(bridgeId: any):Observable<any> {
     /*return new Observable<bridge>(subscriber => {
       subscriber.next(this.generarPuente());
     });*/
-    const bridge = this.generarPuente();
-    return of(bridge)
+    return of(true)
   }
 
   sendInspection(inspection: any) {
