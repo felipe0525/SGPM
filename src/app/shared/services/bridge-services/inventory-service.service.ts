d2cd6948c9564f48f5bbf73284d30203af15fede
parraluisa <parran_ll@javeriana.edu.co>
2024-05-14 09:18:42 -0500

    Inventory service updated adding some methods

--- src/app/shared/services/bridge-services/inventory-service.service.ts
+++ src/app/shared/services/bridge-services/inventory-service.service.ts
@@ -1,19 +1,22 @@
-import {Injectable, inject} from '@angular/core';
+import {inject, Injectable} from '@angular/core';
 import {
   addDoc,
   collection,
   collectionData,
   deleteDoc,
   doc,
+  DocumentData,
+  DocumentReference,
   Firestore,
   getDoc,
+  getDocs,
+  query,
   updateDoc,
-  DocumentReference,
-  DocumentData, where, getDocs, query
+  where
 } from '@angular/fire/firestore';
 import {Inventory} from '../../../models/bridge/inventory';
 import {Observable} from 'rxjs';
-import {getDownloadURL, ref, uploadBytes, Storage, FirebaseStorage} from "@angular/fire/storage";
+import {FirebaseStorage, getDownloadURL, ref, Storage, uploadBytes} from "@angular/fire/storage";
 import {AbstractControl, AsyncValidatorFn, ValidationErrors} from "@angular/forms";
 
 @Injectable({
@@ -37,6 +40,23 @@ export class InventoryServiceService {
       return undefined;
     }
   }
+
+  async getBridgeName(bridgeIdentification: string): Promise<string | undefined> {
+    try {
+      const q = query(this._collection, where('generalInformation.bridgeIdentification', '==', bridgeIdentification));
+      const inventoriesSnapshot = await getDocs(q);
+      if (inventoriesSnapshot.empty) {
+        return undefined;
+      } else {
+        const inventoryData = inventoriesSnapshot.docs[0].data();
+        return inventoryData['generalInformation'].name;
+      }
+    } catch (error) {
+      console.error('Error fetching bridge name:', error);
+      return undefined;
+    }
+  }
+
 
   async createInventory(inventory: Inventory): Promise<DocumentReference<DocumentData>> {
     return await addDoc(this._collection, inventory);
@@ -68,4 +88,29 @@ export class InventoryServiceService {
       return querySnapshot.empty ? null : {bridgeIdentificationNotUnique: true};
     };
   }
+
+  async getBridgeBasicInfo(bridgeId: any) {
+    try {
+      const q = query(this._collection, where('generalInformation.bridgeIdentification', '==', bridgeId));
+      const inventoriesSnapshot = await getDocs(q);
+      if (inventoriesSnapshot.empty) {
+        return undefined;
+      } else {
+        const inventoryData = inventoriesSnapshot.docs[0].data();
+        return {
+          bridgeIdentification: inventoryData['generalInformation'].bridgeIdentification,
+          name: inventoryData['generalInformation'].name,
+          pr: inventoryData['generalInformation'].pr,
+          regional: inventoryData['generalInformation'].regional,
+          regionalIdentification: inventoryData['generalInformation'].regionalIdentification,
+          road: inventoryData['generalInformation'].road,
+          roadIdentification: inventoryData['generalInformation'].roadIdentification,
+        };
+      }
+    } catch (error) {
+      console.error('Error fetching bridge name:', error);
+      return undefined;
+    }
+  }
+
 }
